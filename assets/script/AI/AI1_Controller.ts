f78284f5fd1cb735091e3fdc07a69965fdcaa114
RionaHomePC <2489339078@qq.com>
2025-06-09 21:47:58 +0800

    20250609 2147

---
+++ assets/script/AI/AI1_Controller.ts
@@ -0,0 +1,33 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('AI1_Controller')
+export class AI1_Controller extends Component {
+    // start() {
+
+    // }
+
+    // update(deltaTime: number) {
+        
+    // }
+
+    // 获取数据
+    // 所有位置
+    // 摩擦力
+
+
+    
+    
+
+    // AI传输
+    async AI_action()
+    {
+        
+    }
+
+
+
+
+}
+
+
